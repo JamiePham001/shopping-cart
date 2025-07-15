f314d49f5b0d59b4a205281c6f6d5814bd017a90
Jamie Pham <jamie.pham@outlook.com>
2025-07-15 09:28:57 +0800

    Context API solution link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Footer } from "./components/Footer";
 import "./App.css";
 import { useState, useEffect } from "react";
 
+// Context API solution: https://www.theodinproject.com/lessons/node-path-react-new-managing-state-with-the-context-api
+// a more streamlined solution for sharing data across all levels of components
+// without issues of componenet drilling
+
 function App() {
   const [cart, setCart] = useState(() => {
     // try to load from local storage, else start with an empty array
